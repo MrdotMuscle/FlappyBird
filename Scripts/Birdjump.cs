445a058bd712d511b02ad67436ee17a53a8a7768
MrdotMuscle <54747484+MrdotMuscle@users.noreply.github.com>
2019-11-13 18:54:31 -0500

    Add files via upload

---
+++ Scripts/Birdjump.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Birdjump : MonoBehaviour
+{
+    Rigidbody2D bird;
+    public static int score = 0;
+    public Text scoreText;
+    void Start()
+    {
+        bird = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            bird.velocity = new Vector2(0, 5);
+        }
+        scoreText.text = score.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Point")
+        {
+            score++;
+            return;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Pipes" || collision.gameObject.tag == "Bars")
+        {
+
+            Destroy(gameObject);
+            score = 0;
+            SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 1.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveObstacle();
+    }
+
+    void MoveObstacle()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+}
