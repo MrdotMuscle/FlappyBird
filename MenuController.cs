a10b1422d2729cdaad5014091f4712999f0adaa9
MrdotMuscle <54747484+MrdotMuscle@users.noreply.github.com>
2019-11-13 18:53:12 -0500

    Add files via upload
    FlappyBird

---
+++ MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject playButton;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+ 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+  
+
+    
+
+    
+}
